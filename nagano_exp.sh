54954310adc27846d11ef8ad51992c2b8bd634ed
Nseigou <nagano_1si@yahoo.co.jp>
2024-12-19 17:10:40 +0900

    changing sh

--- nagano_exp.sh
+++ nagano_exp.sh
@@ -4,41 +4,56 @@ TOKEN="uSmbrMW1BuCGlvovL2GlDPXbIoYqvOuMLVFRPcoUkUn"
 
 rm -rf ~/dataTemp
 rm -rf ~/Simulation/* #dataファイルの下を削除する
-mkdir ~/"Simulation/GPSR"
-mkdir ~/"Simulation/NGPSR"
-mkdir ~/"Simulation/NPGPSR"
-mkdir ~/"Simulation/NDGPSR" 
 
 start_time=`date +%s` 
 
 i=1 #loop
 r=2 #実験回数
-
-for protocol in GPSR NGPSR NPGPSR NDGPSR
+for traceFile in mobility37.tcl mobility112.tcl mobility185.tcl
 do
+	if [ $traceFile = "mobility37.tcl" ]; then
+		nodeCout=37
+		simulationTime=360
+	elif [ $traceFile = "mobility112.tcl" ]; then
+		nodeCout=112
+		simulationTime=360
+	elif [ $traceFile = "mobility185.tcl" ]; then
+		nodeCout=185
+		simulationTime=360
+	fi
+	for protocol in GPSR NGPSR NPGPSR NDGPSR
+	do
+		mkdir ~/"Simulation/$traceFile/$protocol"
+		mkdir -p ~/"dataTemp"
+		while [ $i -le $r ]; do
+		
+		echo "-run $i  --RoutingProtocol=$protocol "
+		./waf --run "nagano-sim-alt --protocolName=$protocol --traceFile=$traceFile --nodeCount=$nodeCout --simulationTime=$simulationTime" 
+		
+		mv ~/dataTemp/data.txt ~/dataTemp/data$i.txt
+		mv ~/dataTemp/data$i.txt ~/Simulation/$traceFile/$protocol
 
-	mkdir -p ~/"dataTemp"
-	while [ $i -le $r ]; do
-	
-	echo "-run $i  --RoutingProtocol=$protocol "
-	./waf --run "nagano-sim-alt --protocolName=$protocol "
-	
-	mv ~/dataTemp/data.txt ~/dataTemp/data$i.txt
-	mv ~/dataTemp/data$i.txt ~/Simulation/$protocol
+		# ラインに送信
+		send_line_notification1() { 
+			message="$protocol シミュレーション $i 回目終了"
+			curl -X POST -H "Authorization: Bearer $TOKEN" -F "message=$message" $LINE_NOTIFY_API
+		}	
+		send_line_notification1
 
-    # ラインに送信
-	send_line_notification1() { 
-		message="$protocol シミュレーション $i 回目終了"
+		i=`expr $i + 1`
+		
+		done
+		
+		i=1
+		rm -rf ~/dataTemp
+
+	done
+	# ラインに送信
+	send_line_notification3() { 
+		message="$tranceFile :  終了"
 		curl -X POST -H "Authorization: Bearer $TOKEN" -F "message=$message" $LINE_NOTIFY_API
 	}	
-	send_line_notification1
-
-	i=`expr $i + 1`
-	
-	done
-	
-	i=1
-	rm -rf ~/dataTemp
+	send_line_notification3
 
 done
 
@@ -46,17 +61,17 @@ done
 
 end_time=`date +%s` #unix時刻から現在の時刻までの秒数を取得
 
-  SS=`expr ${end_time} - ${start_time}` #シュミレーションにかかった時間を計算する　秒数
-  HH=`expr ${SS} / 3600` #時を計算
-  SS=`expr ${SS} % 3600`
-  MM=`expr ${SS} / 60` #分を計算
-  SS=`expr ${SS} % 60` #秒を計算
+SS=`expr ${end_time} - ${start_time}` #シュミレーションにかかった時間を計算する　秒数
+HH=`expr ${SS} / 3600` #時を計算
+SS=`expr ${SS} % 3600`
+MM=`expr ${SS} / 60` #分を計算
+SS=`expr ${SS} % 60` #秒を計算
 
-  echo "シュミレーション時間${HH}:${MM}:${SS}" #シミュレーションにかかった時間を　時:分:秒で表示する
+echo "シュミレーション時間${HH}:${MM}:${SS}" #シミュレーションにかかった時間を　時:分:秒で表示する
 
-  send_line_notification2() {
-    message="シミュレーションが終了しました。"
-    curl -X POST -H "Authorization: Bearer $TOKEN" -F "message=$message" $LINE_NOTIFY_API
+send_line_notification2() {
+	message="シミュレーションが終了しました。"
+	curl -X POST -H "Authorization: Bearer $TOKEN" -F "message=$message" $LINE_NOTIFY_API
 }
 
 # LINE通知を送信
