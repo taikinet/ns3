badcfb0bd7a9455b6f444477c141c5af30743c60
Nseigou <nagano_1si@yahoo.co.jp>
2024-12-23 16:41:53 +0900

    add sh

--- nagano_exp.sh
+++ nagano_exp.sh
@@ -124,3 +124,208 @@ for traceFile in mobility112.tcl mobility37_185.tcl mobility74_185.tcl mobility1
 do
     send_line_notification5 $traceFile
 done
+
+
+# 不正ノードあり----------------------------------------------------------------↓
+
+git stash
+TARGET_BRANCH="recent_include-liar"
+git checkout $TARGET_BRANCH
+git pull origin recent_include-liar
+g++ -std=c++11 -o naganomaketable naganomaketable.cc
+
+rm -rf ~/dataTemp
+rm -rf ~/Simulation_liar/* #dataファイルの下を削除する
+
+send_line_notification7() { 
+	traceFile=$1
+    nodeCount=$2
+    simulationTime=$3
+	protocol=$4
+	message="$traceFile,  ノード数：${nodeCount},  simTime : ${simulationTime},  protocol : ${protocol}   終了"
+	curl -X POST -H "Authorization: Bearer $TOKEN" -F "message=$message" $LINE_NOTIFY_API
+}
+
+send_line_notification8() {
+    traceFile=$1
+    file_path="/home/hry-user/Simulation_liar/$traceFile/avarage.txt"
+    if [ -f "$file_path" ]; then
+        message=$(<"$file_path")
+    fi
+    curl -X POST -H "Authorization: Bearer $TOKEN" -F "message=$message" $LINE_NOTIFY_API
+}
+
+start_time=`date +%s` 
+
+i=1 #loop
+r=250 #実験回数   # ここいじる
+nodeCount=0
+simlationTime=0
+send_line_notification1
+for traceFile in mobility112_simTime=250.tcl mobility112_simTime=300.tcl # mobility37.tcl mobility112.tcl mobility185.tcl  ### ここいじる
+do
+	if [ $traceFile == "mobility112_simTime=250.tcl" ]; then
+        fileName="mobility112.tcl"
+		nodeCount=74  
+		simulationTime=250 ### ここいじる
+	elif [ $traceFile == "mobility112_simTime=300.tcl" ]; then
+        fileName="mobility185.tcl"
+		nodeCount=74
+		simulationTime=300
+    
+	fi
+	for protocol in GPSR NGPSR NPGPSR NDGPSR
+	do
+		mkdir -p ~/"Simulation_liar/$traceFile/$protocol"
+		mkdir -p ~/"dataTemp"
+		while [ $i -le $r ]; do
+		
+		echo "-run $i  --RoutingProtocol=$protocol "
+		./waf --run "nagano-sim-alt --protocolName=$protocol --traceFile=/home/hry-user/ns-allinone-3.26/ns-3.26/node/$fileName --nodeCount=$nodeCount --simTime=$simulationTime" 
+		
+		mv ~/dataTemp/data.txt ~/dataTemp/data$i.txt
+		mv ~/dataTemp/data$i.txt ~/Simulation_liar/$traceFile/$protocol
+
+		# ラインに送信	
+		send_line_notification2 "$protocol"
+
+		i=$((i + 1))
+		
+		done
+		
+		i=1
+		rm -rf ~/dataTemp
+
+	done
+		
+	send_line_notification3 "$traceFile" $nodeCount $simulationTime
+
+done
+
+./naganomaketable $r
+
+end_time=`date +%s` #unix時刻から現在の時刻までの秒数を取得
+
+SS=`expr ${end_time} - ${start_time}` #シュミレーションにかかった時間を計算する　秒数
+HH=`expr ${SS} / 3600` #時を計算
+SS=`expr ${SS} % 3600`
+MM=`expr ${SS} / 60` #分を計算
+SS=`expr ${SS} % 60` #秒を計算
+
+echo "シュミレーション時間${HH}:${MM}:${SS}" #シミュレーションにかかった時間を　時:分:秒で表示する
+
+
+# LINE通知を送信
+send_line_notification4 $HH $MM $SS
+
+for traceFile in mobility112_simTime=250.tcl mobility112_simTime=300.tcl # ここいじる
+do
+    send_line_notification8 $traceFile
+done
+
+# simTime=300 ------------------------------------------------------------------------------------------↓
+git stash
+TARGET_BRANCH="recent"
+git checkout $TARGET_BRANCH
+
+rm -rf ~/dataTemp
+rm -rf ~/Simulation300/* #dataファイルの下を削除する
+
+send_line_notification6() {
+    traceFile=$1
+    file_path="/home/hry-user/Simulation300/$traceFile/avarage.txt"
+    if [ -f "$file_path" ]; then
+        message=$(<"$file_path")
+    fi
+    curl -X POST -H "Authorization: Bearer $TOKEN" -F "message=$message" $LINE_NOTIFY_API
+}
+
+
+start_time=`date +%s`
+
+i=1 #loop
+r=250 #実験回数   # ここいじる
+nodeCount=0
+simlationTime=0
+send_line_notification1
+for traceFile in mobility112.tcl mobility37_185.tcl mobility74_185.tcl mobility112_185.tcl mobility150_185.tcl mobility185.tcl   # mobility37.tcl mobility112.tcl mobility185.tcl  ### ここいじる
+do
+	if [ $traceFile == "mobility112.tcl" ]; then
+        fileName="mobility112.tcl"
+		nodeCount=74  
+		simulationTime=300 ### ここいじる
+	elif [ $traceFile == "mobility37_185.tcl" ]; then
+        fileName="mobility185.tcl"
+		nodeCount=37
+		simulationTime=300
+    elif [ $traceFile == "mobility74_185.tcl" ]; then
+        fileName="mobility185.tcl"
+		nodeCount=74
+		simulationTime=300
+    elif [ $traceFile == "mobility112_185.tcl" ]; then
+        fileName="mobility185.tcl"
+		nodeCount=112
+		simulationTime=300
+    elif [ $traceFile == "mobility150_185.tcl" ]; then
+        fileName="mobility185.tcl"
+		nodeCount=150
+		simulationTime=300
+    elif [ $traceFile == "mobility185.tcl" ]; then
+        fileName="mobility185.tcl"
+		nodeCount=185
+		simulationTime=300
+	fi
+	for protocol in GPSR NGPSR NPGPSR NDGPSR
+	do
+		mkdir -p ~/"Simulation300/$traceFile/$protocol"
+		mkdir -p ~/"dataTemp"
+		while [ $i -le $r ]; do
+		
+		echo "-run $i  --RoutingProtocol=$protocol "
+		./waf --run "nagano-sim-alt --protocolName=$protocol --traceFile=/home/hry-user/ns-allinone-3.26/ns-3.26/node/$fileName --nodeCount=$nodeCount --simTime=$simulationTime" 
+		
+		mv ~/dataTemp/data.txt ~/dataTemp/data$i.txt
+		mv ~/dataTemp/data$i.txt ~/Simulation300/$traceFile/$protocol
+
+		# ラインに送信	
+		send_line_notification2 "$protocol"
+
+		i=$((i + 1))
+		
+		done
+
+		send_line_notification7 "$traceFile" $nodeCount $simulationTime $protocol
+		
+		i=1
+		rm -rf ~/dataTemp
+
+	done
+		
+	send_line_notification3 "$traceFile" $nodeCount $simulationTime
+
+done
+
+./naganomaketable2 $r
+
+end_time=`date +%s` #unix時刻から現在の時刻までの秒数を取得
+
+SS=`expr ${end_time} - ${start_time}` #シュミレーションにかかった時間を計算する　秒数
+HH=`expr ${SS} / 3600` #時を計算
+SS=`expr ${SS} % 3600`
+MM=`expr ${SS} / 60` #分を計算
+SS=`expr ${SS} % 60` #秒を計算
+
+echo "シュミレーション時間${HH}:${MM}:${SS}" #シミュレーションにかかった時間を　時:分:秒で表示する
+
+
+# LINE通知を送信
+send_line_notification4 $HH $MM $SS
+
+for traceFile in mobility112.tcl mobility37_185.tcl mobility74_185.tcl mobility112_185.tcl mobility150_185.tcl mobility185.tcl # ここいじる
+do
+    send_line_notification6 $traceFile
+done
+
+
+
+
---
+++ naganomaketable2.cc
@@ -0,0 +1,238 @@
+#include <stdlib.h>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+#include <string>
+#include <time.h>
+#include <vector>
+
+using namespace std;
+
+
+int ReturnStrings(const char* path, const int num, char* buf, const int size)
+{
+    int cnt = 0;
+    FILE* file = NULL;
+
+    // ファイルが存在しない、開けないならばエラー
+    if ((file = fopen(path, "r")) == NULL)
+        return -1;
+
+    while (fgets(buf, size, file) != NULL && num > ++cnt);
+    fclose(file);
+
+    if (num != cnt)
+    {
+        for (cnt = 0; cnt < size; ++cnt)
+            buf[cnt] = 0;
+        return -2;
+    }
+
+    return 0;
+}
+
+int makeFile(int t){
+
+    char GPSR_0[128];
+    char NGPSR_0[128];
+    char PGPSR_0[128];
+    char DGPSR_0[128];
+    double avg_GPSR;
+    double avg_NGPSR;
+    double avg_PGPSR;
+    double avg_DGPSR;
+    double sum_GPSR = 0.0;
+    double sum_NGPSR = 0.0;
+    double sum_PGPSR = 0.0;
+    double sum_DGPSR = 0.0;
+    double max_GPSR = 0.0;  // 最初の値で初期化
+    double max_NGPSR = 0.0;
+    double max_PGPSR = 0.0;
+    double max_DGPSR = 0.0;
+    double min_GPSR = 1000000000;  // 最初の値で初期化
+    double min_NGPSR = 10000000000;
+    double min_PGPSR = 10000000000;
+    double min_DGPSR = 10000000000;
+
+	
+	std::vector<std::string> traceFiles = {"mobility112.tcl", "mobility37_185.tcl", "mobility74_185.tcl", "mobility112_185.tcl", "mobility150_185.tcl", "mobility185.tcl"};    // ここいじる
+    for (const std::string& traceFile : traceFiles) {
+
+
+        double GPSR[t],NGPSR[t],PGPSR[t],DGPSR[t];
+        std::string value = "NULL";	
+
+        fstream fs;
+        fs.open("/home/hry-user/Simulation300/"+traceFile+"/avarage.txt", ios::out | ios::app);
+        fs << traceFile << endl;
+        fs << std::to_string(t) << "回の平均" << ", " << "GPSR" << ", " << "NGPSR" << ", " << "NPGPSR" << ", " << "NDGPSR" << endl;
+        fs.close();
+        
+        for(int i=1;i<=10;i++){
+
+            sum_GPSR = 0;
+            sum_NGPSR = 0;
+            sum_PGPSR = 0;
+            sum_DGPSR = 0;
+            max_GPSR = 0.0;  // 最初の値で初期化
+            max_NGPSR = 0.0;
+            max_PGPSR = 0.0;
+            max_DGPSR = 0.0;
+            min_GPSR = 1000000000;  // 最初の値で初期化
+            min_NGPSR = 10000000000;
+            min_PGPSR = 10000000000;
+            min_DGPSR = 10000000000;
+
+
+            if (i < 9){
+                for(int l=1;l<=t;l++){
+                
+                
+                    std::string Gstr = "/home/hry-user/Simulation300/"+traceFile+"/GPSR/data" +std::to_string(l)+ ".txt";
+                    std::string Istr = "/home/hry-user/Simulation300/"+traceFile+"/NGPSR/data" +std::to_string(l)+ ".txt";
+                    std::string Hstr = "/home/hry-user/Simulation300/"+traceFile+"/NPGPSR/data" +std::to_string(l)+ ".txt";
+                    std::string Dstr = "/home/hry-user/Simulation300/"+traceFile+"/NDGPSR/data" +std::to_string(l)+ ".txt";
+
+                    
+                    
+                    ReturnStrings( Gstr.c_str(), i, GPSR_0, 128 );
+                    ReturnStrings( Istr.c_str(), i, NGPSR_0, 128 );
+                    ReturnStrings( Hstr.c_str(), i, PGPSR_0, 128 );
+                    ReturnStrings( Dstr.c_str(), i, DGPSR_0, 128 );
+
+                    
+                
+                    GPSR[l] = atof(GPSR_0);
+                    NGPSR[l] = atof(NGPSR_0);
+                    PGPSR[l] = atof(PGPSR_0);
+                    DGPSR[l] = atof(DGPSR_0);
+                }
+
+                switch(i){		
+                    case 1:
+                    value="Throughput";
+                    break;
+                    case 2:
+                    value="PDR";
+                    break;
+                    case 3:
+                    value="Overhead";
+                    break;
+                    case 4:
+                    value="Delay";
+                    break;
+                    case 5:
+                    value="Loss";
+                    break;
+                    case 6:
+                    value="HopNum";	
+                    break;
+                    case 7:
+                    value="SimTime";
+                    break;
+                    case 8:
+                    value="Memory";
+                    break;
+                    default:
+                    value="NULL";			
+                }
+                fstream fs;
+                fs.open("/home/hry-user/Simulation300/"+traceFile+"/"+value+".txt", ios::out);
+                fs << value<<","<<"GPSR"<<","<<"NGPSR"<<","<<"NPGPSR"<<","<<"NDGPSR"<<endl;
+                
+                for(int r=1; r<=t; r++){
+                    fs <<std::to_string(r)<<"回目"<<","<<std::to_string(GPSR[r])<<","<<std::to_string(NGPSR[r])<<","<<std::to_string(PGPSR[r])<<","<<std::to_string(DGPSR[r])<<endl;
+                    sum_GPSR += GPSR[r];
+                    sum_NGPSR += NGPSR[r];
+                    sum_PGPSR += PGPSR[r];
+                    sum_DGPSR += DGPSR[r];
+                    if (GPSR[r] > max_GPSR) max_GPSR = GPSR[r];
+                    if (NGPSR[r] > max_NGPSR) max_NGPSR = NGPSR[r];
+                    if (PGPSR[r] > max_PGPSR) max_PGPSR = PGPSR[r];
+                    if (DGPSR[r] > max_DGPSR) max_DGPSR = DGPSR[r];
+                    if (GPSR[r] < min_GPSR) min_GPSR = GPSR[r];
+                    if (NGPSR[r] < min_NGPSR) min_NGPSR = NGPSR[r];
+                    if (PGPSR[r] < min_PGPSR) min_PGPSR = PGPSR[r];
+                    if (DGPSR[r] < min_DGPSR) min_DGPSR = DGPSR[r];
+
+                }
+                fs.close();	
+
+                avg_GPSR = sum_GPSR / t;
+                avg_NGPSR = sum_NGPSR / t;
+                avg_PGPSR = sum_PGPSR / t;
+                avg_DGPSR = sum_DGPSR / t;
+
+                fs.open("/home/hry-user/Simulation300/"+traceFile+"/avarage.txt", ios::out | ios::app);
+                fs << value << ", " << avg_GPSR << ", " << avg_NGPSR << ", " << avg_PGPSR << ", " << avg_DGPSR << endl;
+                fs << "最大値" << ", " << max_GPSR << ", " << max_NGPSR << ", " << max_PGPSR << ", " << max_DGPSR << endl;
+                fs << "最小値" << ", " << min_GPSR << ", " << min_NGPSR << ", " << min_PGPSR << ", " << min_DGPSR << endl;
+                fs << "--------------------------------" << endl;
+                fs.close();
+
+            } else {
+                for(int l=1;l<=t;l++){
+                
+                    std::string Hstr = "/home/hry-user/Simulation300/"+traceFile+"/NPGPSR/data" +std::to_string(l)+ ".txt";
+                    std::string Dstr = "/home/hry-user/Simulation300/"+traceFile+"/NDGPSR/data" +std::to_string(l)+ ".txt";
+
+                    ReturnStrings( Hstr.c_str(), i, PGPSR_0, 128 );
+                    ReturnStrings( Dstr.c_str(), i, DGPSR_0, 128 );
+
+                    PGPSR[l] = atof(PGPSR_0);
+                    DGPSR[l] = atof(DGPSR_0);
+                }
+
+                switch(i){		
+                    case 9:
+                    value="GenerateSignatureTime";
+                    break;
+                    case 10:
+                    value="VerifySignatureTime";
+                    break;
+                    default:
+                    value="NULL";			
+                }
+
+                fstream fs;
+                fs.open("/home/hry-user/Simulation300/"+traceFile+"/"+value+".txt", ios::out);
+                fs << value<<","<<"NPGPSR"<<","<<"NDGPSR"<<endl;
+                
+                for(int r=1; r<=t; r++){
+                    fs <<std::to_string(r)<<"回目"<<","<<std::to_string(PGPSR[r])<<","<<std::to_string(DGPSR[r])<<endl;
+                    sum_PGPSR += PGPSR[r];
+                    sum_DGPSR += DGPSR[r];
+                    if (PGPSR[r] > max_PGPSR) max_PGPSR = PGPSR[r];
+                    if (DGPSR[r] > max_DGPSR) max_DGPSR = DGPSR[r];
+                    if (PGPSR[r] < min_PGPSR) min_PGPSR = PGPSR[r];
+                    if (DGPSR[r] < min_DGPSR) min_DGPSR = DGPSR[r];
+                }
+                fs.close();	
+
+                avg_PGPSR = sum_PGPSR / t;
+                avg_DGPSR = sum_DGPSR / t;
+
+                fs.open("/home/hry-user/Simulation300/"+traceFile+"/avarage.txt", ios::out | ios::app);
+                fs << value << ", " << "署名なし" << ", " << "署名計算なし" << ", " << avg_PGPSR << ", " << avg_DGPSR << endl;
+                fs << "最大値" << ", " << "署名なし" << ", " << "署名計算なし" << ", " << max_PGPSR << ", " << max_DGPSR << endl;
+                fs << "最小値" << ", " << "署名なし" << ", " << "署名計算なし" << ", " << min_PGPSR << ", " << min_DGPSR << endl;
+                fs << "--------------------------------" << endl;
+                fs.close();
+                    
+            }
+        }
+    }
+	return 0;
+}
+
+
+int main(int argc, char *argv[]){
+
+	int t = atoi(argv[1]);
+	makeFile(t);
+	
+	
+	return 0;
+
+}
